2decda791fa96d90169a3125864650799fdf1742
Vishnu Narayanan V <100830902+VishnuNV007@users.noreply.github.com>
2022-08-11 20:01:34 +0530

    Add files via upload

---
+++ list_content.py
@@ -0,0 +1,9 @@
+#Python program to write a list content to a file.
+
+color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+with open('abc.txt', "w") as myfile:
+        for c in color:
+                myfile.write("%s\n" % c)
+
+content = open('python1.txt')
+print(content.read())
